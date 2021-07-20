aa50c69ea65a6f4c46de2ba8e716d2596761e137
bcentner <bcentner@umich.edu>
2021-07-20 16:08:57 -0400

    test this

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
         setTimeout(()=> {
-            logoSpan.forEach((span, idx) => {
+            logo.forEach((span, idx) => {
                 setTimeout(() => {
                     span.classList.remove('active');
                     span.classList.add('fade');
